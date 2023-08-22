92c97c99819f4b8998e367078c2c9fd2fb52158c
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-22 00:38:58 -0300

    feat: add next auth providers components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { NextAuthProvider } from "@/providers/auth";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
@@ -19,7 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <NextAuthProvider>{children}</NextAuthProvider>
+      </body>
     </html>
   );
 }
