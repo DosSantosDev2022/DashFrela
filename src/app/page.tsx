4f7622f0d5fec8f86ef4cd1e187edc5e1bbfd7b1
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-22 23:14:45 -0300

    fix: fixing styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,44 +9,44 @@ import CheckInput from "@/components/inputs/checkBox";
 
 export default function Home() {
   return (
-    <div className="flex  justify-center items-center">
-      <div className="w-[364px] h-[500px] border p-8">
+    <div className="flex h-screen  justify-center items-center">
+      <div className="w-[364px] h-auto border rounded-xl shadow-md p-8">
         <div className=" flex flex-col gap-3">
           <LogoDark />
           <h4 className="text-sm font-normal text-MyColor02">
             Insira suas informações de login
           </h4>
         </div>
-        <div className="flex flex-col gap-5 mt-3">
-          <Input label="Usuário" />
-          <Input label="Senha" />
+        <form className="flex flex-col gap-5 mt-3">
+          <Input label="Usuário" type={"text"} />
+          <Input label="Senha" type={"password"} />
           <CheckInput text={"Lembre-me"} />
-        </div>
-        <Button className="w-full mt-5" variant="primary">
-          Login
-        </Button>
-        <Button
-          className=" flex items-center justify-center gap-3 w-full h-10 mt-5"
-          variant="outlined"
-        >
-          Login com Google
-          <FaGoogle />
-        </Button>
-        <div className="flex justify-around mt-3">
-          <Link
-            className="text-sm text-MyColor02 font-normal hover:underline"
-            href={""}
+          <Button className="w-full mt-2" variant="primary">
+            Login
+          </Button>
+          <Button
+            className=" flex items-center justify-center gap-3 w-full h-10"
+            variant="outlined"
           >
-            Criar sua conta
-          </Link>
-          <Link
-            className="text-sm text-MyColor02 font-normal hover:underline"
-            href={""}
-          >
-            {" "}
-            Esqueceu sua senha ?
-          </Link>
-        </div>
+            Login com Google
+            <FaGoogle />
+          </Button>
+          <div className="flex justify-around mt-2">
+            <Link
+              className="text-sm text-MyColor02 font-normal hover:underline"
+              href={""}
+            >
+              Criar sua conta
+            </Link>
+            <Link
+              className="text-sm text-MyColor02 font-normal hover:underline"
+              href={""}
+            >
+              {" "}
+              Esqueceu sua senha ?
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
