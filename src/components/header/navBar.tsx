6b8cd25b34e870dfc9eee45a547104ee2fd4b688
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-26 16:19:08 -0300

    fix: alterando tag html

--- src/components/header/navBar.tsx
+++ src/components/header/navBar.tsx
@@ -4,17 +4,17 @@ import LogoDark from "../logo/logo";
 
 export default function NavBar() {
   return (
-    <header className="w-full h-[72px} py-4 px-16 flex justify-between items-center  border-b border-MyColor03">
+    <nav className="w-full h-[72px} py-4 px-16 flex justify-between items-center  border-b border-MyColor03">
       <LogoDark />
-      <div className="flex gap-2">
+      <div className="flex items-center gap-2">
         <Link href={"/login"}>
           <Button>Login</Button>
         </Link>
         <Link href={"/cadastro"}>
-          <Button variant="outlined">Cadastrar</Button>
+          <Button variant="outlined">Cadastro</Button>
         </Link>
       </div>
-    </header>
+    </nav>
   );
 }
 
