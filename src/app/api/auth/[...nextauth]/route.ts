2fc054aceaa9f8fe406ba8f9fd737ec3a429b050
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-28 23:03:46 -0300

    fix : fixing google privider route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,14 +11,9 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      
     }),
   ],
-  callbacks: {
-    async session ({session,token,user}){
-      session.user = {... session.user, id: user.id} as {id:string; name: string; email:string}
-      return session
-    }
-  }
 };
 
 const handler = NextAuth(authOptions);
