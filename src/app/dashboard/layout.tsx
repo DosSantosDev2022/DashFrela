a6e12b89e316973c49a3d127566ac8fa4a221cad
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-25 23:09:49 -0300

    styles: alterando estilos do layout dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,14 +8,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="grid grid-cols-12 p-2 h-screen">
+    <section className="grid grid-cols-12 p-2 w-screen h-screen">
       <div className="col-span-2 top-0 left-0 h-full">
         <SideBar />
       </div>
       <div className="col-span-10 relative">
         <HeaderDash />
-        <main className="bg-slate-600 p-4 absolute top-16 left-0 right-0 bottom-0 overflow-y-auto">
-          <div className="border p-2 rounded-lg h-full">{children}</div>
+        <main className="bg-slate-100 p-4 absolute top-16 left-0 right-0 bottom-0 overflow-y-auto">
+          <div className="p-2 rounded-lg h-screen">{children}</div>
         </main>
       </div>
     </section>
