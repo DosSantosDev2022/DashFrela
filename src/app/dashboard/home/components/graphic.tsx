c96eb9a71bc5130e435885667ed6e40b4f2dde91
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-26 00:42:58 -0300

    feat: criando componente resumo graficos

---
+++ src/app/dashboard/home/components/graphic.tsx
@@ -0,0 +1,9 @@
+const PerformanceGraphic = () => {
+  return (
+    <>
+      <div className="bg-white p-4 w-[477px] h-[457px] shadow-md rounded-md "></div>
+    </>
+  );
+};
+
+export default PerformanceGraphic;
