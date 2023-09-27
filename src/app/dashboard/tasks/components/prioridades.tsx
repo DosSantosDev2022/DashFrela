afcceef56d91ee5140f4a0eca610edf0deb3a290
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-27 00:32:42 -0300

    feat: criando componente para prioridades

---
+++ src/app/dashboard/tasks/components/prioridades.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface PrioridadeTasksProps {
+  namePriority: string;
+}
+
+const PrioridadeTasks = ({ namePriority }: PrioridadeTasksProps) => {
+  // Função para determinar a classe de cor com base no nome da prioridade
+  const getClassBasedOnPriority = (prioridade: string) => {
+    switch (prioridade) {
+      case "Prioridade Alta":
+        return "bg-red-500";
+      case "Prioridade Média":
+        return "bg-amber-400";
+      case "Prioridade Baixa":
+        return "bg-green-500";
+      default:
+        return "bg-gray-400";
+    }
+  };
+
+  // Obtém a classe de cor com base na prioridade
+  const corClasse = getClassBasedOnPriority(namePriority);
+
+  return (
+    <p className={`p-1 text-xs font-normal text-white rounded-md ${corClasse}`}>
+      {namePriority}
+    </p>
+  );
+};
+
+export default PrioridadeTasks;
