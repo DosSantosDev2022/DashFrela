b46755a0e2b7217a5196d1348d798208c8b1f424
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-27 00:33:05 -0300

    feat: criando página para tarefas

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -1,7 +1,49 @@
+import { FaClipboardCheck } from "react-icons/fa6";
+import CardBackground from "../components/CardBackground ";
+import CreateItens from "../components/createItens";
+import TaskCards from "./components/taskCards";
+
 const TasksPage = () => {
   return (
     <>
-      <h1>TasksPage</h1>
+      <div className="flex flex-col gap-3">
+        <CreateItens name={"Minhas tarefas"} nameButton={"Criar Tarefas"} />
+
+        <div className=" flex gap-3">
+          <CardBackground className={"w-80 h-32"}>
+            <TaskCards
+              name={"Nome projeto"}
+              icon={<FaClipboardCheck />}
+              btnName={"Abrir"}
+              namePriority={"Prioridade Alta"}
+            />
+          </CardBackground>
+          <CardBackground className={"w-80 h-32"}>
+            <TaskCards
+              name={"Nome projeto"}
+              icon={<FaClipboardCheck />}
+              btnName={"Abrir"}
+              namePriority={"Prioridade Média"}
+            />
+          </CardBackground>
+          <CardBackground className={"w-80 h-32"}>
+            <TaskCards
+              name={"Nome projeto"}
+              icon={<FaClipboardCheck />}
+              btnName={"Abrir"}
+              namePriority={"Prioridade Baixa"}
+            />
+          </CardBackground>
+          <CardBackground className={"w-80 h-32"}>
+            <TaskCards
+              name={"Nome projeto"}
+              icon={<FaClipboardCheck />}
+              btnName={"Abrir"}
+              namePriority={"Prioridade Alta"}
+            />
+          </CardBackground>
+        </div>
+      </div>
     </>
   );
 };
