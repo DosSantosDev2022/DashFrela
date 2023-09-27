6f5dbf61a47c68203a29a8744ea3aaaff6afaa33
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-26 22:36:46 -0300

    feat: criando página projetos

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import Button from "@/components/buttons/button";
+import CardBackground from "../components/CardBackground ";
+
 const ProjectsPage = () => {
   return (
-    <>
-      <h1>Projects Page</h1>
-    </>
+    <div className="flex flex-col gap-3">
+      <div className="flex items-center justify-between p-6 border">
+        <p>Meus Projetos</p>
+        <Button>Criar Projeto</Button>
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-between p-6">
+        <CardBackground className={""}>
+          {/* Cards que listam os projetos criados pelo usuário */}
+          <p>Nome do projeto</p>
+          <div className="flex items-center gap-2">
+            <Button>Abrir</Button>
+            <Button variant="danger">Excluir</Button>
+          </div>
+        </CardBackground>
+      </div>
+    </div>
   );
 };
 
