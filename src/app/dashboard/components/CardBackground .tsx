e83b961d815cfad6d2e8c7d314e770408c416869
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-26 22:36:05 -0300

    feat : construindo component cardbackground

---
+++ src/app/dashboard/components/CardBackground .tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+interface CardBackgroundProps {
+  children: ReactNode;
+  className: string;
+}
+
+const CardBackground = ({ children, className }: CardBackgroundProps) => {
+  const cardClasses = `bg-white rounded-md p-6 shadow-lg w-full flex justify-between items-center ${
+    className || ""
+  }`;
+
+  return <div className={cardClasses}>{children}</div>;
+};
+
+export default CardBackground;
