93301dd6507cbc5fd35c2f72c56cd9f5da917a15
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-28 22:09:52 -0300

    styles: alterando estilos da página

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -3,15 +3,20 @@
 import Button from "@/components/buttons/button";
 import CardBackground from "../components/CardBackground ";
 import CreateItens from "../components/createItens";
+import PriorityTag from "../tasks/components/priorityTags";
 
 const ProjectsPage = () => {
   return (
     <div className="flex flex-col gap-3">
       <CreateItens name={"Meus projetos"} nameButton={"Criar Projeto"} />
-      <div className="flex flex-col gap-4 items-center justify-between p-6">
+
+      <div className="flex flex-col gap-4 items-center justify-between p-6 border">
         <CardBackground className={""}>
           {/* Cards que listam os projetos criados pelo usuário */}
           <p>Nome do projeto</p>
+          <p>Nome Cliente</p>
+          <PriorityTag namePriority={"Prioridade Alta"} />
+          <PriorityTag namePriority={"Em andamento"} />
           <div className="flex items-center gap-2">
             <Button>Abrir</Button>
             <Button variant="danger">Excluir</Button>
