56bb49e4ae6bd86d50db9bf131c52682a7ee2ca3
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-29 00:53:04 -0300

    fix: corrigindo component createitens

--- src/app/dashboard/components/createItens.tsx
+++ src/app/dashboard/components/createItens.tsx
@@ -1,11 +1,13 @@
 import Button from "@/components/buttons/button";
+import { ReactElement } from "react";
 
 interface CreateItensProps {
   name: string;
   nameButton: string;
+  button?: ReactElement;
 }
 
-const CreateItens = ({ name, nameButton }: CreateItensProps) => {
+const CreateItens = ({ name, nameButton, button }: CreateItensProps) => {
   return (
     <div className="flex items-center justify-between p-6 border">
       <p className="text-MyColor02 font-medium text-lg">{name}</p>
