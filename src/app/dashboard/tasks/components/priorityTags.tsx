4232df35ae56f26ac58e79967e91aa4e4b81a855
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-28 22:08:03 -0300

    feat: criando novos case para componente tags

--- src/app/dashboard/tasks/components/priorityTags.tsx
+++ src/app/dashboard/tasks/components/priorityTags.tsx
@@ -8,11 +8,19 @@ const PriorityTag = ({ namePriority }: PrioridadeTasksProps) => {
   // Função para determinar a classe de cor com base no nome da prioridade
   const getClassBasedOnPriority = (prioridade: string) => {
     switch (prioridade) {
+      /* Prioridades Tags */
       case "Prioridade Alta":
         return "bg-red-500";
-      case "Prioridade Média":
+      case "Prioridade média":
         return "bg-amber-400";
       case "Prioridade Baixa":
+        return "bg-green-500";
+      /* Status tags */
+      case "Pendente":
+        return "bg-red-500";
+      case "Em andamento":
+        return "bg-amber-400";
+      case "Concluído":
         return "bg-green-500";
       default:
         return "bg-gray-400";
