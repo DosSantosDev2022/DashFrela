df9b6c1a728b7320419dc073d62581490f0ddccf
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-09-29 00:55:14 -0300

    feat : criando componente label

---
+++ src/app/dashboard/configs/components/label.tsx
@@ -0,0 +1,14 @@
+interface LabelProps {
+  htmlFor: string;
+  children: React.ReactNode;
+}
+
+const Label: React.FC<LabelProps> = ({ htmlFor, children }) => {
+  return (
+    <label htmlFor={htmlFor} className="text-base font-medium text-MyColor02">
+      {children}
+    </label>
+  );
+};
+
+export default Label;
