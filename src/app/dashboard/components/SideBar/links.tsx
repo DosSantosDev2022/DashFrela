b6f91b6a6c6944703ba64b8bd9de5eb4c466e24c
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-11-25 16:28:06 -0300

    refatorando componente sidebar e links

--- src/app/dashboard/components/SideBar/links.tsx
+++ src/app/dashboard/components/SideBar/links.tsx
@@ -1,18 +1,42 @@
 import Link from 'next/link'
-import { ReactNode } from 'react'
 
-interface NavLinksPros {
-  icon: ReactNode
-  name: string
-  href: string
-}
+import { MdSpaceDashboard } from 'react-icons/md'
 
-const NavLinks = ({ icon, name, href }: NavLinksPros) => {
+const NavLinks = () => {
+  const links = [
+    { nome: 'Dashboard', href: '/dashboard/home', icon: <MdSpaceDashboard /> },
+    {
+      nome: 'Projetos',
+      href: '/dashboard/projects',
+      icon: <MdSpaceDashboard />,
+    },
+    { nome: 'Tarefas', href: '/dashboard/tasks', icon: <MdSpaceDashboard /> },
+    {
+      nome: 'Clientes',
+      href: '/dashboard/clients',
+      icon: <MdSpaceDashboard />,
+    },
+    {
+      nome: 'Configurações',
+      href: '/dashboard/configs',
+      icon: <MdSpaceDashboard />,
+    },
+  ]
+
   return (
-    <li className="text-white list-none flex items-center gap-2 p-1 hover:bg-slate-100 hover:text-MyColor01 transition-all hover:scale-105 rounded-md ">
-      <i>{icon}</i>
-      <Link href={href}>{name}</Link>
-    </li>
+    <nav>
+      <ul className="flex flex-col gap-6 h-full">
+        {links.map((link) => (
+          <li
+            key={link.nome}
+            className="text-white list-none flex items-center gap-2 p-1 cursor-pointer hover:bg-slate-100 hover:text-MyColor01 transition-all hover:scale-105 rounded-md "
+          >
+            <i>{link.icon}</i>
+            <Link href={link.href}>{link.nome}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
--- src/app/dashboard/components/SideBar/sidebar.tsx
+++ src/app/dashboard/components/SideBar/sidebar.tsx
@@ -1,40 +1,13 @@
 import Image from 'next/image'
 
-import { MdSpaceDashboard } from 'react-icons/md'
 import NavLinks from './links'
 
 const SideBar = () => {
   return (
-    <div className="w-60 h-full  bg-MyColor01 p-6 flex flex-col  gap-6 rounded-l-lg ">
+    <aside className="w-60 h-full  bg-MyColor01 p-6 flex flex-col  gap-6 rounded-l-lg ">
       <Image width={150} height={50} src={'/Logo-White.svg'} alt={''} />
-      <nav className="flex flex-col gap-6 h-full">
-        <NavLinks
-          icon={<MdSpaceDashboard />}
-          name={'DashBoard'}
-          href={'/dashboard/home'}
-        />
-        <NavLinks
-          icon={<MdSpaceDashboard />}
-          name={'Projetos'}
-          href={'/dashboard/projects'}
-        />
-        <NavLinks
-          icon={<MdSpaceDashboard />}
-          name={'Tarefas'}
-          href={'/dashboard/tasks'}
-        />
-        <NavLinks
-          icon={<MdSpaceDashboard />}
-          name={'Clientes'}
-          href={'/dashboard/clients'}
-        />
-        <NavLinks
-          icon={<MdSpaceDashboard />}
-          name={'Configurações'}
-          href={'/dashboard/configs'}
-        />
-      </nav>
-    </div>
+      <NavLinks />
+    </aside>
   )
 }
 
