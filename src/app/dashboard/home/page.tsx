c17edabadcc5072bb24c6756cb116261758ce9f0
Juliano Santos <119906141+DosSantosDev2022@users.noreply.github.com>
2024-03-21 10:25:13 +0000

    fix : refatorando pagina home do dashboard

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,56 +1,5 @@
-import {
-  FaClipboardCheck,
-  FaLocationCrosshairs,
-  FaRegWindowRestore,
-  FaUserLarge,
-} from 'react-icons/fa6'
-import CardsDash from '@/components/dashboard/pages/home/cards'
-
-import PerformanceGraphic from '@/components/dashboard/pages/home/graphic'
-import Teams from '@/components/dashboard/pages/home/teams'
-import { ProjectListResum } from '@/components/dashboard/pages/home/projectListResum'
-
 const PageDashboard = () => {
-  return (
-    <div className="flex- flex-col h-full space-y-14 p-5">
-      <div className="flex items-center justify-between gap-4">
-        <CardsDash
-          name={'Projetos'}
-          icon={<FaRegWindowRestore />}
-          content={'50'}
-          text={'8'}
-          iconColor="#624BFF"
-        />
-        <CardsDash
-          name={'Tarefas Ativas'}
-          icon={<FaClipboardCheck />}
-          content={'50'}
-          text={'5'}
-          iconColor="#0DCAF0"
-        />
-        <CardsDash
-          name={'Clientes'}
-          icon={<FaUserLarge />}
-          content={'50'}
-          text={'3'}
-          iconColor="#DC3545"
-        />
-        <CardsDash
-          name={'Produtividade'}
-          icon={<FaLocationCrosshairs />}
-          content={'50%'}
-          text={'4%'}
-          iconColor="#28A745"
-        />
-      </div>
-      <ProjectListResum />
-
-      <div className="flex items-start justify-around gap-5">
-        <PerformanceGraphic /> {/* Componente do gráfico de performance */}
-        <Teams /> {/* Componente com resumo dos membros do time */}
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default PageDashboard
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { NextAuthProvider } from '@/providers/auth'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 
@@ -9,11 +8,6 @@ const poppins = Poppins({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
 })
-
-export const metadata: Metadata = {
-  title: 'DashFreela',
-  description: 'O seu painel adm para',
-}
 
 export default function RootLayout({
   children,
